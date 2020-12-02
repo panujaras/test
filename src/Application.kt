1471968234267c568f54401501e27986b43a53a2
panujaras14 <panujaras14@gmail.com>
2020-12-02 22:12:37 +0700

    user success

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,37 @@
 package com.test
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+// ipconfig
 fun Application.module() {
-    // TODO: 12/2/2020  test
+
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    install(Routing) {
+
+        get(path = "/api/see/user") {
+            val user = User(name = "see", age = 22)
+            call.respond(user)
+        }
+
+    }
+
 }
 
+data class User(
+    var name: String? = null,
+    var age: Int? = null,
+)
